dfb8a72b36bb19e515933f9e7e91a53f4bf208a8
ArthurHwang <ahwang55@gmail.com>
2020-11-15 16:05:29 -0800

    cleanup

--- src/pages/Episodes.js
+++ src/pages/Episodes.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-// import { useLocation } from 'react-router-dom';
 import { EpisodeCard } from '../components/EpisodeCard';
 import { useEffect, useState } from 'react';
 
